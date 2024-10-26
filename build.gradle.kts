b5fee2a7e7f04ceda165757281e621d53ae64df9
Soham Basu <sohambasu@outlook.com>
2024-10-27 02:57:22 +0530

    spotless apply

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,21 @@
-plugins {
-    id("com.diffplug.spotless") version("6.25.0")
-}
+plugins { id("com.diffplug.spotless") version ("6.25.0") }
 
-repositories {
-    gradlePluginPortal()
-}
+repositories { gradlePluginPortal() }
 
 spotless {
+  java {
+    target("**/*.java")
+    googleJavaFormat()
+    formatAnnotations()
+  }
 
-    java {
-        target("**/*.java")
-        googleJavaFormat()
-        formatAnnotations()
-    }
+  kotlin {
+    target("**/*.kt")
+    ktfmt().googleStyle()
+  }
 
-    kotlin {
-        target("**/*.kt")
-        ktfmt().googleStyle()
-    }
-
-    kotlinGradle {
-        target("**/*.gradle.kts")
-        ktfmt().googleStyle()
-    }
+  kotlinGradle {
+    target("**/*.gradle.kts")
+    ktfmt().googleStyle()
+  }
 }
--- buildSrc/src/main/kotlin/java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/java-conventions.gradle.kts
@@ -1,34 +1,25 @@
-plugins {
-  java
-}
+plugins { java }
 
 repositories {
   mavenCentral()
-    gradlePluginPortal()
+  gradlePluginPortal()
 }
 
-java {
-  toolchain {
-    languageVersion.set(JavaLanguageVersion.of(21))
-  }
-}
+java { toolchain { languageVersion.set(JavaLanguageVersion.of(21)) } }
 
 dependencies {
   compileOnly("org.projectlombok:lombok:1.18.34")
-	annotationProcessor("org.projectlombok:lombok:1.18.34")
-	
-	testCompileOnly("org.projectlombok:lombok:1.18.34")
-	testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
+  annotationProcessor("org.projectlombok:lombok:1.18.34")
+
+  testCompileOnly("org.projectlombok:lombok:1.18.34")
+  testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
 
   testImplementation(platform("org.junit:junit-bom:5.11.3"))
-	testImplementation("org.junit.jupiter:junit-jupiter")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  testImplementation("org.junit.jupiter:junit-jupiter")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
-
 
 tasks.test {
   useJUnitPlatform()
-	testLogging {
-		events("passed", "skipped", "failed")
-	}
+  testLogging { events("passed", "skipped", "failed") }
 }
